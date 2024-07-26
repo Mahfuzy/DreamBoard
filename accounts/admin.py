3f91244a631814750daa6ee37f9d5ac61a7679d5
Mahfuzy <seidumahfuz18@gmail.com>
2024-07-26 12:31:35 +0000

    update accouts/admin.py

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,6 +1,26 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from .models import User, Follow
 
+class CustomUserAdmin(UserAdmin):
+    model = User
+    list_display = ('username', 'email', 'is_active', 'is_staff', 'is_superuser', 'last_login', 'profile_pic')
+    list_filter = ('is_active', 'is_staff', 'is_superuser')
+    
+    fieldsets = (
+        (None, {'fields': ('username', 'email', 'password', 'profile_pic', 'bio', 'website')}),
+        ('Permissions', {'fields': ('is_staff', 'is_active', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Dates', {'fields': ('last_login', 'date_joined')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'email', 'password1', 'password2', 'is_staff', 'is_active', 'profile_pic', 'bio', 'website')}
+        ),
+    )
+    
+    search_fields = ('email', 'username')
+    ordering = ('email',)
 
-admin.site.register(User)
+admin.site.register(User, CustomUserAdmin)
 admin.site.register(Follow)
