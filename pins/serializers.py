11aca09889632af139c121db9c476f687eb3f182
Mahfuzy <seidumahfuz18@gmail.com>
2024-08-01 15:57:49 +0000

    something

--- pins/serializers.py
+++ pins/serializers.py
@@ -24,7 +24,6 @@ class SavePinsSerializer(serializers.ModelSerializer):
             validated_data['user'] = request.user
         return super().create(validated_data)
 
-
 class CommentRepliesSerializer(serializers.ModelSerializer):
     user = UserSerializer(read_only=True)
     replies = serializers.SerializerMethodField()
@@ -53,6 +52,7 @@ class CommentSerializer(serializers.ModelSerializer):
         
     def get_replies_count(self, obj):
         return obj.replies.count()
+
     def create(self, validated_data):
         request = self.context.get('request', None)
         if request:
@@ -65,19 +65,20 @@ class CommentSerializer(serializers.ModelSerializer):
             data['replies'] = []  # If there are no replies, return an empty list
         return data
 
-
 class PinSerializer(serializers.ModelSerializer):
-    user =  UserSerializer(read_only=True)
+    user = UserSerializer(read_only=True)
     comments = CommentSerializer(many=True, read_only=True)
     comments_count = serializers.SerializerMethodField()
     likes_count = serializers.SerializerMethodField()
     saves_count = serializers.SerializerMethodField()
+    
     class Meta:
         model = Pin
         fields = [
             'id', 'title', 'user', 'description', 'link', 'board', 'image', 'video', 'date_created',
              'comments', 'comments_count', 'likes_count', 'saves_count'
         ]
+    
     def create(self, validated_data):
         request = self.context.get('request', None)
         if request:
@@ -86,13 +87,14 @@ class PinSerializer(serializers.ModelSerializer):
     
     def get_comments_count(self, obj):
         return obj.comments.count()
+    
     def get_likes_count(self, obj):
         return obj.likes.count()
-
+    
     def get_saves_count(self, obj):
         return obj.savepins_set.count()
-
+    
     def validate(self, data):
-        if data['title'] == '':
+        if not data.get('title'):
             raise serializers.ValidationError("Title cannot be empty.")
         return data
