b636d38a0b14a98085e8e0c521f2522963fed188
Mahfuzy <seidumahfuz18@gmail.com>
2024-08-17 17:47:25 +0000

    fixed things

--- DreamBoard/asgi.py
+++ DreamBoard/asgi.py
@@ -2,18 +2,19 @@ import os
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 from channels.auth import AuthMiddlewareStack
-from channels.security.websocket import AllowedHostsOriginValidator
+from channels.security.websocket import OriginValidator
 from chat.routing import websocket_urlpatterns
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DreamBoard.settings')
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
-    "websocket": AllowedHostsOriginValidator(
+    "websocket": OriginValidator(
         AuthMiddlewareStack(
             URLRouter(
                 websocket_urlpatterns
             )
-        )
+        ),
+        ['https://dreamboard.onrender.com']
     ),
 })
--- DreamBoard/settings.py
+++ DreamBoard/settings.py
@@ -103,7 +103,9 @@ MIDDLEWARE = [
 
 CSRF_COOKIE_SECURE = True 
 
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    'https://dreamboard.onrender.com',
+]
 
 CORS_ALLOW_CREDENTIALS = True
 
